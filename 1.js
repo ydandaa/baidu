7404273b46da3b52e712e58cd58eec33cff0521d
zzzz <1479526265@qq.com>
2016-09-23 11:19:28 +0800

    保存下拉细节

--- 1.js
+++ 1.js
@@ -2,10 +2,11 @@
     l.onclick=function(){ 
    	    var l=document.getElementById('l');
         var enter=document.getElementById('enter');
-        enter.style.display='block';	 
+        enter.style.visibility='visible';	
+        return false; 
    }
    var btn=document.getElementById('btn');
    btn.onclick=function(){
-   	   enter.style.display='none';
+   	   enter.style.visibility='hidden';
    }
 }(window))
